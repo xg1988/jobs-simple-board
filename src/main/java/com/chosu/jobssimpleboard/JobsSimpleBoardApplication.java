97767304a02c4427ee473d4e5f017421a41009bc
joseong-ug <hoho88@naver.com>
2023-10-15 12:55:00 +0900

    Initial commit

---
+++ src/main/java/com/chosu/jobssimpleboard/JobsSimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.chosu.jobssimpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsSimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsSimpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chosu/jobssimpleboard/JobsSimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chosu.jobssimpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsSimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
